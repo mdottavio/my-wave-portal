7880afe18ebd294765913e6408951d4c9caed07a
Mauricio Dottavio <dottavio@gmail.com>
2021-11-28 11:10:21 -0300

    feat: section 1 - last step

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,25 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    uint256 lastWaveDate;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Starting contract ", block.timestamp);
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        lastWaveDate = block.timestamp;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getLastWaveDate() public view returns (uint256) {
+      return lastWaveDate;
     }
 }
