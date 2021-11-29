4ab3a0a0365b3386246d9577c42a932d1753100d
Mauricio Dottavio <dottavio@gmail.com>
2021-11-29 02:21:12 -0300

    fix: removing debugg

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,5 @@
 import contractAbi from "./contractAbi.json";
 
-console.log(process.env);
-
 const config = {
   CONTRACT_ADDRESS: process.env.REACT_APP_CONTRACT_ADDRESS,
   GIPHY_KEY: process.env.REACT_APP_GIPHY_KEY,
