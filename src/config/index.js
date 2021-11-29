22a73707584b1ab311a180bce37a27dfef30e203
Mauricio Dottavio <dottavio@gmail.com>
2021-11-29 02:16:18 -0300

    fix: debuggin env vars

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 import contractAbi from "./contractAbi.json";
 
+console.log(process.env);
+
 const config = {
   CONTRACT_ADDRESS: process.env.REACT_APP_CONTRACT_ADDRESS,
   GIPHY_KEY: process.env.REACT_APP_GIPHY_KEY,
